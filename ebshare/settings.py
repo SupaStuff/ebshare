3a0c0e82cb86d4b6dd08ded7c6c5a63951000d32
Moustafa Elshaabiny <moustafa.elshaabiny@gmail.com>
2015-12-12 17:52:09 -0500

    static_root modified

--- ebshare/settings.py
+++ ebshare/settings.py
@@ -130,7 +130,7 @@ ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = 'staticfiles'
+#STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
