b648147cc5a8dd7564fb77700acc4649e3ee8cda
Moustafa Elshaabiny <moustafa.elshaabiny@gmail.com>
2015-12-12 18:06:17 -0500

    staticfiles directory fixed

--- ebshare/settings.py
+++ ebshare/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "ebshare/static"),
 )
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
