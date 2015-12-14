3fe5c90ec481bfcaa1f96b110c469d835dcab27d
AbhishekSamdaria <samdaria@bxscience.edu>
2015-12-14 06:19:52 -0500

    Associated books button and HTML

--- books/views.py
+++ books/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import render_to_response
 from books.models import book
 from django.http import HttpResponseRedirect, HttpResponse
 
-from django.template import RequestContext
-from django.core.urlresolvers import reverse
-
 # Create your views here.
 
 def index(request):
@@ -26,22 +23,16 @@ def renderbookshelf(request):
 
 def contribute_book(request):
 	if request.method == 'POST':
-		form = DocumentForm(request.POST, request.FILES)
-		
-		if form.is_valid():
-			book1 = books(book_text = request.FILES['book_text'])
-
-		if book1.book_text.content_type == "text/plain":
-			book(book_title=request.POST['title'],
-				user=request.user,
-				book_author=request.POST['author'],
-				description = request.POST['description'],
-				details = request.POST['detail'],
-				cover=request.FILES['cover_image'],
-				alt_text=request.POST['alt_text'],
-				genre=request.POST['genre'],
-				book_text=request.FILES['book_text']
-			).save()
-			return HttpResponseRedirect('/bookshelf')
+		book(book_title=request.POST['title'],
+			user=request.user,
+			book_author=request.POST['author'],
+			description = request.POST['description'],
+			details = request.POST['detail'],
+			cover=request.FILES['cover_image'],
+			alt_text=request.POST['alt_text'],
+			genre=request.POST['genre'],
+			book_text=request.FILES['book_text']
+		).save()
+		return HttpResponseRedirect('/bookshelf')
 	else:
 		return render(request, "books/upload.html", {})
